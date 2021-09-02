65d2c8e56a0ab702aa24d3f2ec494610dfa9fc4e
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-09-02 16:51:47 +0530

    Updated

--- src/test/java/com/mindtree/employee/storeemployee/StoreEmployeeApplicationTests.java
+++ src/test/java/com/mindtree/employee/storeemployee/StoreEmployeeApplicationTests.java
@@ -3,7 +3,9 @@ package com.mindtree.employee.storeemployee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.mindtree.customer.storecustomer.StoreCustomerApplication;
+
+@SpringBootTest(classes = StoreCustomerApplication.class)
 class StoreEmployeeApplicationTests {
 
 	@Test
